323dfc5c276688922f821e50e6c9ee054b98705f
Peter Gombos <pepegombos@gmail.com>
2026-01-22 21:07:23 +0100

    setup shadcn and drizzle.

---
+++ src/db/index.ts
@@ -0,0 +1,16 @@
+import { drizzle } from "drizzle-orm/libsql";
+
+if (!process.env.TURSO_DATABASE_URL) {
+  throw new Error("TURSO_DATABASE_URL environment variable is not set");
+}
+
+if (!process.env.TURSO_AUTH_TOKEN) {
+  throw new Error("TURSO_AUTH_TOKEN environment variable is not set");
+}
+
+export const db = drizzle({
+  connection: {
+    url: process.env.TURSO_DATABASE_URL,
+    authToken: process.env.TURSO_AUTH_TOKEN,
+  },
+});
